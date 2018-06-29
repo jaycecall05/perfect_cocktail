63537e72e9578641219853380ee4b62ad8388014
Jayce <callrj5@gmail.com>
2018-06-28 18:51:08 -0600

    built the Results Template

--- js/UI.js
+++ js/UI.js
@@ -1,5 +1,51 @@
 class UI {
 
+
+
+    displayDrinksWithIngredients(drinks) {
+        const resultsWrapper = document.querySelector('.results-wrapper');
+        resultsWrapper.style.display = 'block';
+
+
+        //insert results
+        const resultsDiv = document.querySelector('#results');
+
+        drinks.forEach(drink => {
+            resultsDiv.innerHTML += `
+            
+                <div class="col-md-6">
+                    <div class="card my-3">
+                        <img class="card-img-top" src="https://${drink.strDrinkThumb}" alt="${drink.strDrink}">
+                
+                        <div class="card-body">
+                            <h2 class="card-title text-center"></h2>
+                            <p class="card-text font-weight-bold">Instructions: </p>
+                            <p class="card-text">
+
+                            </p>
+                            <p class ="card-text">
+                                <ul class="list-group">    
+                                    <li class="list-group-item alert
+                                    alert-danger">Ingredients</li>
+
+                                <ul>
+                            </p>
+                            <p class="card-text font-weight-bold">Exta Information</p>
+                            <p class="card-text">
+                                <span class="badge badge-pill badge-success">
+
+                                </span>
+                                <span class="badge badge-pill badge-warning">
+                                    Category:
+                                </span>
+                        </div>
+                    </div>
+                </div>
+            
+
+            `;
+        })
+    }
     // show a custome message
     printMessage(message, className) {
         const div = document.createElement('div');
